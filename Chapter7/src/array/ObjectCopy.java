ded0887ceb44841133156dc10732c6f613df2151
KangHyunGyu <dhddifl@gmail.com>
2021-06-17 14:58:57 +0900

    Chapter7.1

--- Chapter7/src/array/ObjectCopy.java
+++ Chapter7/src/array/ObjectCopy.java
@@ -11,24 +11,46 @@ public class ObjectCopy {
 		bookArray1[1] = new Book("태백산맥2", "조정래");
 		bookArray1[2] = new Book("태백산맥3", "조정래");
 		
-		System.arraycopy(bookArray1, 0, bookArray2, 0, 3);
+		bookArray2[0] = new Book();
+		bookArray2[1] = new Book();
+		bookArray2[2] = new Book();
 		
-		for(int i =0; i<bookArray2.length; i++) {
-			bookArray2[i].showBookInfo();
+		//System.arraycopy(bookArray1, 0, bookArray2, 0, 3);
+		
+		for(int i =0; i<bookArray1.length; i++) {
+			bookArray2[i].setAuthor(bookArray1[i].getAuthor());
+			bookArray2[i].setBookName(bookArray1[i].getBookName());
 		}
 		
 		bookArray1[0].setBookName("나목");
 		bookArray1[0].setAuthor("박완서");
 		
+		/*
 		for(int i =0; i<bookArray1.length; i++) {
+		 
 			bookArray1[i].showBookInfo();
 		}
 		
 		System.out.println("--------------------");
 		
 		for(int i =0; i<bookArray1.length; i++) {
-			bookArray1[i].showBookInfo();
+			bookArray2[i].showBookInfo();
 		}
+		*/
+		
+		//향상된for문
+		String[] strArr = {"Java" , "Android" , "C"};
+		
+		for(String s : strArr) {
+			System.out.println(s);
+		}
+		
+		int [] arr = {1,2,3,4,5};
+
+		for(int num : arr) { //num = arr[];
+			System.out.println(num);
+		}
+		
 	}
 
 }
