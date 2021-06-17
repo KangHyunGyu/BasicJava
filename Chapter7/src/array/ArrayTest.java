6783a8b8d5951cc6895c7a73ea12773cc16696ea
KangHyunGyu <dhddifl@gmail.com>
2021-06-17 13:35:44 +0900

    Chapter7

---
+++ Chapter7/src/array/ArrayTest.java
@@ -0,0 +1,30 @@
+package array;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+
+
+		int[] numbers = new int[] {1,2,3};
+		
+		//numbers = new int[3];
+		//numbers = new int [] {1,2,3};
+		//
+		
+		//numbers[0] = 1;
+		//numbers[1] = 2;
+		//numbers[2] = 3;
+		
+		for(int i=0; i<numbers.length; i++ ) {
+			System.out.println(numbers[i]);
+		}
+		
+		//int[] studentIDs = new int [5];
+		//for(int i=0; i<studentIDs.length; i++ ) {
+		//	System.out.println(studentIDs[i]);
+		//}
+		
+		
+	}
+
+}
